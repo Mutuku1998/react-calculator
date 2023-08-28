6bdf9b6048f5d84b25a9970d9793f4c442dcd1e9
mutuku1998 <martinmutuku1998@gmail.com>
2023-08-28 17:14:53 +0300

    adding useReducer hook

--- rcalc/src/App.js
+++ rcalc/src/App.js
@@ -1,16 +1,43 @@
 
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
 import './App.css';
 import './style.css'
+export const ACTIONS ={
+ ADD_DIGITS:'add-digits',
+ CLEAR:'clear',
+ DELETE_DIGIT:'delete-digit',
+ CHOOSE_OPERATION:'choose-operation',
+ EVALUATE : 'evaluate',
+
+}
+  
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGITS:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
+      };
+    // Other cases...
+    default:
+      return state;
+  }
+}
 function App() {
+  const [{currentOperand,previousOperand ,operation}, dispatch] = useReducer(reducer, 
+    {})
+    dispatch({type: ACTIONS.ADD_DIGITS,payload:{digit:1}})
   return (
     <div className="calculator-grid">
       <div className='output'>
-        <div className='previous-operand'> 34 + 445 </div>
-<div className='current-operand'>44,48</div>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+<div className='current-operand'>{currentOperand}</div>
       </div>
      <button className='span-two' >AC</button>
      <button>DEL</button>  
-     <button>/</button>
+     <DigitButton digit='/'dispatch={dispatch}/>
      <button>1</button>
      <button>2</button>
      <button>3</button>
